10526710b17b0488916718f3a82e3f0949913eda
Manuel Aguirre <softwagz@gmail.com>
2020-03-12 10:34:47 -0500

    Update Index

--- src/app/Servicios/clientes.service.ts
+++ src/app/Servicios/clientes.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import {AngularFirestore} from '@angular/fire/firestore';
-import { NgForm } from '@angular/forms';
 
 
 @Injectable({
--- src/app/componentes/proveedores/proveedores.component.ts
+++ src/app/componentes/proveedores/proveedores.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { AngularFireAuth } from '@angular/fire/auth';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-proveedores',
@@ -9,13 +10,16 @@ import { AngularFireAuth } from '@angular/fire/auth';
 })
 export class ProveedoresComponent implements OnInit {
 
-  constructor(private toas:ToastrService,private auth:AngularFireAuth) { }
+  constructor(private toas: ToastrService, private auth: AngularFireAuth) { }
 
   ngOnInit() {
+/*  */
+
   }
-  singOut(){
+  singOut() {
     this.auth.auth.signOut();
-    this.toas.show('Su sesion ha finalizado correctamente','Cerrando Session');
+    this.toas.show('Su sesion ha finalizado correctamente', 'Cerrando Session');
   }
+
 
 }
